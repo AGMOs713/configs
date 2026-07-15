f154b38cf670178ba127b01f4e1adc69bf7a9023
AGMOs713 <arthurgmo@hotmail.com>
2026-07-14 23:19:50 -0300

    Fixed errors in the scripts

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,41 +2,41 @@
 
 if [ -f ~/.bash_aliases ]; then
     mv ~/.bash_aliases ~/.bash_aliases.old
-    cp ./.bash_aliases ~/
 fi
+cp ./.bash_aliases ~/
 
 if [ -f ~/.bash_logout ]; then
     mv ~/.bash_logout ~/.bash_logout.old
-    cp ./.bash_logout ~/
 fi
+cp ./.bash_logout ~/
 
 if [ -f ~/.bash_prompt ]; then
     mv ~/.bash_prompt ~/.bash_prompt.old
-    cp ./.bash_prompt ~/
 fi
+cp ./.bash_prompt ~/
 
 if [ -f ~/.bashrc ]; then
     mv ~/.bashrc ~/.bashrc.old
-    cp ./.bashrc ~/
 fi
+cp ./.bashrc ~/
 
 if [ -f ~/.git-prompt.sh ]; then
     mv ~/.git-prompt.sh ~/.git-prompt.sh.old
-    cp ./.git-prompt.sh ~/
 fi
+cp ./.git-prompt.sh ~/
 
 if [ -f ~/.nanorc ]; then
     mv ~/.nanorc ~/.nanorc.old
-    cp ./.nanorc ~/
 fi
+cp ./.nanorc ~/
 
 if [ -d ~/.nano-syntax ]; then
     mv ~/.nano-syntax ~/.nano-syntax.old
-    cp -r ./.nano-syntax ~/
 fi
+cp -r ./.nano-syntax ~/
 
 
 if [ -f ~/.config/ghostty/config ]; then
     mv ~/.config/ghostty/config ~/.config/ghostty/config.old
-    cp ./.config/ghostty/config ~/
 fi
+cp ./.config/ghostty/config ~/
--- scripts/restore.sh
+++ scripts/restore.sh
@@ -1,42 +1,74 @@
 #!/bin/sh
 
-if [ -f ~/.bash_aliases ] && [ -f ~/.bash_aliases.old ]; then
-    rm ~/.bash_aliases
+if [ -f ~/.bash_aliases ]; then
+    :
+elif [ -f ~/.bash_aliases.old ]; then
+    rm -f ~/.bash_aliases
     mv ~/.bash_aliases.old ~/.bash_aliases
+else
+    :
 fi
 
-if [ -f ~/.bash_logout ] && [ -f ~/.bash_logout.old ]; then
-    rm ~/.bash_logout
+if [ -f ~/.bash_logout ]; then
+    :
+elif [ -f ~/.bash_logout.old ]; then
+    rm -f ~/.bash_logout
     mv ~/.bash_logout.old ~/.bash_logout
+else
+    :
 fi
 
-if [ -f ~/.bash_prompt ] && [ -f ~/.bash_prompt.old ]; then
-    rm ~/.bash_prompt
+if [ -f ~/.bash_prompt ]; then
+    :
+elif [ -f ~/.bash_prompt.old ]; then
+    rm -f ~/.bash_prompt
     mv ~/.bash_prompt.old ~/.bash_prompt
+else
+    :
 fi
 
-if [ -f ~/.bashrc ] && [ -f ~/.bashrc.old ]; then
-    rm ~/.bashrc
+if [ -f ~/.bashrc ]; then
+    :
+elif [ -f ~/.bashrc.old ]; then
+    rm -f ~/.bashrc
     mv ~/.bashrc.old ~/.bashrc
+else
+    :
 fi
 
-if [ -f ~/.git-prompt.sh ] && [ -f ~/.git-prompt.sh,old ]; then
-    rm ~/.git-prompt.sh
+if [ -f ~/.git-prompt.sh ]; then
+    :
+elif [ -f ~/.git-prompt.sh.old ]; then
+    rm -f ~/.git-prompt.sh
     mv ~/.git-prompt.sh.old ~/.git-prompt.sh
+else
+    :
 fi
 
-if [ -f ~/.nanorc ] && [ -f ~/.nanorc.old ]; then
-    rm ~/.nanorc
+if [ -f ~/.nanorc ]; then
+    :
+elif [ -f ~/.nanorc.old ]; then
+    rm -f ~/.nanorc
     mv ~/.nanorc.old ~/.nanorc
+else
+    :
 fi
 
-if [ -d ~/.nano-syntax ] && [ -d ~/.nano-syntax.old ]; then
-    rm -r ~/.nano-syntax
+if [ -d ~/.nano-syntax ]; then
+    :
+elif [ -d ~/.nano-syntax.old ]; then
+    rm -rf ~/.nano-syntax
     mv ~/.nano-syntax.old ~/.nano-syntax
+else
+    :
 fi
 
 
-if [ -f ~/.config/ghostty/config ] && [ -f ~/.config/ghostty/config.old ]; then
-    rm ~/.config/ghostty/config
+if [ -f ~/.config/ghostty/config ]; then
+    :
+elif [ -f ~/.config/ghostty/config.old ]; then
+    rm -f ~/.config/ghostty/config
     mv ~/.config/ghostty/config.old ~/.config/ghostty/config
+else
+    :
 fi
