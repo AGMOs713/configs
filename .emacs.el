7db0205bef5994b62fc367dce9e57650c0c145dd
AGMOs713 <arthurgmo@hotmail.com>
2026-07-20 11:58:35 -0300

    Added move-text.el

--- .emacs.el
+++ .emacs.el
@@ -29,6 +29,10 @@
 (require 'simpc-mode)
 (add-to-list 'auto-mode-alist '("\\.[hc]\\(pp\\)?\\'" . simpc-mode))
 
+(require 'move-text)
+(global-set-key (kbd "M-<up>") 'move-text-up)
+(global-set-key (kbd "M-<down>") 'move-text-down)
+
 ;; custom functions (
 ;; agm-duplicate-line (
 
